0f72969fab552dca78f4eee8dc03e7f87a2a4ff2
jaseoca <jaseoca@gmail.com>
2023-06-08 20:30:49 +1000

    final edits

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # joca8822.A3.github.io
 assessment 3 tracker web application 
+
+This is a very simplified version of the initial mockup with a more bare-bones functionality. The user form was kept intact and has the same variables but has been turned into a collapsable to be more seamless in a single-page front-end architecture.
+
+
+I was also initially deciding on whether to use a Spotify API or rely on manual input in my mockup, a critique that was given to me was that this was unclear in my data model. For this prototype, it is entirely manual input.
+
+
+Aesthetically, it remains quite similar with a mainly basic and contrasty black/white theme with a dark blue to red gradient for the 'add listing' tab. A lot of the UI however was simplified. For example, the profile section and signout buttons have been removed due to no need for user authentication. Most of the sort-by functions have not been implemented either.
+
+
+So in total, the data model properties that did not make it into the mockup include year, genre category, and image.
+In hindsight, this was more of an organisational and time management problem.
+
+
+Also with the UI, I kept the cards but changed the internal layout. It is entirely text-based whereas the mockup was mostly image based. This was due to not using the API. The app allows users to input basic song data and have it displayed in the form of a card. Other functionalities only enable adding more listings or removing current listings.
+
+
+References 
+
+Dongas. R. 2023. Scrimba. "Task Tracker"
+https://scrimba.com/scrim/coa064ba08c96c1639bc9531b
+
+W3 SCHOOLS
+Containers. https://www.w3schools.com/w3css/w3css_containers.asp
+Buttons. https://www.w3schools.com/tags/tag_button.asp
+Forms. https://www.w3schools.com/html/html_forms.asp
+
+FONT
+adobe fonts. https://fonts.adobe.com/
+'Roc Grotesk'
+
+AI ASSISTANCE
+chatgpt. https://openai.com/blog/chatgpt
+
+What I used AI for:
+- Syntax: Proofreading my code for errors I may of missed. 
+- Variables: I was also having problems with being able to display all of my variables in a listing and used it to help with a solution with my script. 
+- Cards: I needed help with a solution on how to input my variables into a card and how to make them work with flexbox. 
+- Helping find a solution to enabling local storage when using the app.
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,43 +1,105 @@
+
+//The form variable represents the HTML form element with the ID "songform".//
+//The songlistElem variable represents the HTML element with the ID "songList", which is the container where the song listings will be displayed.
+//The songList variable is an array that will store the song objects entered through the form.
+
 const form = document.getElementById("songform");
-const tasklistElem = document.querySelector('#songlist');
+const songlistElem = document.querySelector('#songlist');
+var songList = [];
+
+//enabling the app to store listings locally between sessions. Listens to when the user loads/reloads the page
+window.addEventListener('load', function() {
+  const storedSongList = localStorage.getItem('songList');
+  if (storedSongList) {
+    songList = JSON.parse(storedSongList);
+    songList.forEach(displayListing);
+  }
+});
+
+//An event listener is added for the 'submit' button in the form. The code is executed when the user inputs all necessary values "song, artist, album" and hits submit. 
+//The values entered into the form are then retrieved and stored in variables. These variables are used for the addTask function when it is executed. 
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
 
-  console.log(form.elements.artistName.value);
-  addTask(
-    form.elements.artistName.value,
-    form.elements.songTitle.value,
-    form.elements.albumName.value,
-    form.elements.comments.value
+  // Retrieve values from the form inputs
+  const artist = form.elements.artist.value;
+  const song = form.elements.song.value;
+  const album = form.elements.album.value;
+  const comments = form.elements.comments.value;
+  const quantity = form.elements.quantity.value;
 
-  );
+  addTask(artist, song, album, comments, quantity);
+
+  //resetting the form after submission is completed
   form.reset();
 });
 
 function displayListing(task) {
-  let item = document.createElement('li');
-  item.innerHTML = `<p>${task.artist} - ${task.song}</p>`;
-  songlist.appendChild(item);
+  let item = document.createElement('div');
+  item.classList.add('card');
+  //The 'card' item is created after the form is submitted. It follows the same hierarchy of the form with the addition of a "delete" button to remove current listings.
+  //PLEASE NOTE: When launched in a default browser, after the form is submitted it may display as a dotpoint and not fully function properly. 
+  //Launching the file in with the live server plugin gets rid of this problem and the script runs as intended. 
+  item.innerHTML = `
+    <div class="container">
+      <h2>Artist: ${task.artist}</h2>
+      <p>Song: ${task.song}</p>
+      <p>Album: ${task.album}</p>
+      <p>Comments: ${task.comments}</p>
+      <p>Rating: ${task.quantity}</p>
+      <button class="delete-button">Remove</button>
+    </div>
+  `;
+
+//event listener for when the remove button is pressed
+  const deleteButton = item.querySelector('.delete-button');
+  deleteButton.addEventListener('click', function() {
+    removeListing(task.id);
+    item.remove();
+  });
+
+  songlistElem.appendChild(item);
 }
 
-var songList = [];
+//function for the remove button. Called when the remove button is pressed. 
+function removeListing(id) {
+  songList = songList.filter(task => task.id !== id);
+//using local storage to recognise when a listing has been previously deleted. 
+  localStorage.setItem('songList', JSON.stringify(songList));
+}
 
-function addTask(artist, song, album, comments) {
+//Called when the forum text input is given valid values. Also generated an ID and date added when submitted. 
+function addTask(artist, song, album, comments, quantity) {
   let task = {
-    artist, 
+    artist,
     song,
     id: Date.now(),
     date: new Date().toISOString(),
     album,
-    comments
+    comments,
+    quantity
   };
 
   songList.push(task);
+  //displayListing shows the newly created listing card under the favourites page
   displayListing(task);
+
+  console.log(songList);
+
+  //using local storage to store the variables submitted to 'songlist'.
+  localStorage.setItem('songList', JSON.stringify(songList));
 }
 
-console.log(songList);
+
+
+
+
+
+
+
+
+
 
 
 
